2fc6c87b094fdf2dc3c9b58d0c32d1c91a313d12
age16 <jobiersack@gmail.com>
2025-07-08 01:48:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -11,3 +12,7 @@ def signal():
     return jsonify({
         "message": "Tes sinyal AI XAU/USD aktif"
     })
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
