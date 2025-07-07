5041809e59fd522f8164a0bd3da9fb07a08f27a7
age16 <jobiersack@gmail.com>
2025-07-08 01:10:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api/signal', methods=['GET'])
-def get_signal():
-    signal = {
+@app.route("/")
+def home():
+    return "Hello from XAU/USD AI Server!"
+
+@app.route("/api/signal")
+def signal():
+    return jsonify({
         "message": "🤖 AI Signal XAU/USD\n📈 Sinyal: BUY\n💰 Harga: 3310.45\n🎯 TP: 3318.00\n⛔ SL: 3302.00\n🧠 Confidence: 92%"
-    }
-    return jsonify(signal)
+    })
