0416642d00b234b2c24e372287008078e35bb173
age16 <jobiersack@gmail.com>
2025-07-08 01:16:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from XAU/USD AI Server!"
+    return "✅ AI Signal Server Aktif!"
 
 @app.route("/api/signal")
 def signal():
     return jsonify({
         "message": "🤖 AI Signal XAU/USD\n📈 Sinyal: BUY\n💰 Harga: 3310.45\n🎯 TP: 3318.00\n⛔ SL: 3302.00\n🧠 Confidence: 92%"
     })
+
+# ⛔ Jangan pakai app.run()
